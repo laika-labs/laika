59864bdba4c307e8428465b4c92d0b4e90d42cfb
Nithi Nomprawat <0xnithi@gmail.com>
2023-10-17 21:21:33 +0700

    feat: setup tailwindcss, setup project alias

---
+++ src/pages/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="text-red-700">hello laika</p>
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
+  },
 })
