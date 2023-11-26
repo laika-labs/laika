bf6838b37ad114ca4f6d28ea59986cf9441c98e2
Nithi Nomprawat <0xnithi@gmail.com>
2023-11-26 22:19:53 +0700

    feat: add tooltip on the EVM collections icon

--- src/pages/EVM/index.tsx
+++ src/pages/EVM/index.tsx
@@ -3,27 +3,40 @@ import { Folders } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { useEVMTabStore } from '@/store/tabs'
 
 import Collections from './Collections'
+import ContractRequest from './ContractRequest'
 
 export default function EVM() {
+  const { tabs } = useEVMTabStore()
   return (
     <Allotment proportionalLayout={false}>
       <Allotment.Pane minSize={256} maxSize={376} preferredSize={320} priority={LayoutPriority.Low} snap>
         <Tabs defaultValue="collections" orientation="vertical" className="w-full h-full">
           <Allotment>
             <Allotment.Pane minSize={48} maxSize={48}>
-              <TabsList className="p-2 bg-inherit">
-                <TabsTrigger
-                  value="collections"
-                  className="data-[state=active]:text-primary data-[state=active]:shadow-none"
-                  asChild
-                >
-                  <Button variant="ghost" className="h-auto p-2">
-                    <Folders className="w-4 h-4" />
-                  </Button>
-                </TabsTrigger>
-              </TabsList>
+              <TooltipProvider>
+                <TabsList className="p-2 bg-inherit">
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <TabsTrigger
+                        value="collections"
+                        className="data-[state=active]:text-primary data-[state=active]:shadow-none"
+                        asChild
+                      >
+                        <Button variant="ghost" className="h-auto p-2">
+                          <Folders className="w-4 h-4" />
+                        </Button>
+                      </TabsTrigger>
+                    </TooltipTrigger>
+                    <TooltipContent side="right">
+                      <p>Collections</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TabsList>
+              </TooltipProvider>
             </Allotment.Pane>
             <Allotment.Pane>
               <TabsContent value="collections" className="w-full h-full m-0">
@@ -33,7 +46,7 @@ export default function EVM() {
           </Allotment>
         </Tabs>
       </Allotment.Pane>
-      <Allotment.Pane priority={LayoutPriority.High}> </Allotment.Pane>
+      <Allotment.Pane priority={LayoutPriority.High}>{tabs.length > 0 && <ContractRequest />}</Allotment.Pane>
     </Allotment>
   )
 }
