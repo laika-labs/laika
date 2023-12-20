1ac4d8ef8aa8cc8a91770bd329418986544eed9b
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2023-12-20 09:34:41 +0700

    chore: revise welcome page

--- src/pages/EVM/Welcome/Details.tsx
+++ src/pages/EVM/Welcome/Details.tsx
@@ -1,34 +1,13 @@
-import { Button } from '@/components/ui/button'
-import { useCallback } from 'react'
-import { FolderOpen } from 'lucide-react'
+import laikaLogo from '/laika-labs.svg'
 
 const Details = () => {
-  const handleClickMenuButton = useCallback((topicInput: string) => {
-    console.log(topicInput)
-  }, [])
-
   return (
     <>
+      <img src={laikaLogo} className="w-32 h-32" alt="Laika Labs logo" />
       <p className="text-3xl font-semibold">Laika</p>
       <p className="pt-2 font-light text-md">All your data is saved locally in you browser!</p>
-      <div className="flex space-x-1 pt-5 text-base">
-        <Button
-          variant="default"
-          className="hover:underline"
-          onClick={() => handleClickMenuButton('Create a new Collection')}
-        >
-          <FolderOpen />
-        </Button>
-        <Button
-          variant="secondary"
-          className="hover:underline"
-          onClick={() => handleClickMenuButton('Create a new Smart Contract')}
-        >
-          Create a new Smart Contract
-        </Button>
-      </div>
-      <p className="mt-4 text-sm text-gray-600">Laika 🐶 is Open Source</p>
-      <p className="mt-4 text-sm text-gray-600">
+      <p className="mt-4 text-sm text-gray-600 mb-0">Laika 🐶 is 100% Open Source</p>
+      <p className="mt-4 text-sm text-gray-600 mt-0">
         Give us a Star ⭐️ on{' '}
         <a className="underline" href="https://github.com/laika-labs/laika" target="_blank" rel="noopener noreferrer">
           GitHub
