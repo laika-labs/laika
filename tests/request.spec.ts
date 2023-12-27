cdd930821063bdd8c8329119bf04ecf83ac3296f
Yuttakhan B. <yuttakhanbaingen@gmail.com>
2023-12-27 18:05:12 +0700

    test: add test case for create contract request

--- tests/request.spec.ts
+++ tests/request.spec.ts
@@ -1,27 +1,49 @@
-import { describe } from 'node:test'
 import { User, UserNote, useAppActors } from './fixtures/actors'
-import { it } from '@serenity-js/playwright-test'
+import { beforeEach, describe, it } from '@serenity-js/playwright-test'
 import { Navigate } from '@serenity-js/web'
 import { Ensure, equals } from '@serenity-js/assertions'
 import { notes } from '@serenity-js/core'
 import { laikaWebAppUrl } from './fixtures/constants'
 import {
   ClickCreateNewRequestDialog,
+  FillAbi,
   FillContractAddrInput,
   SelectChain,
   SubmitCreateContractRequest,
 } from './app/create-request/tasks'
 import { contractAddressInput, selectedChain } from './app/create-request/questions'
 
-describe('Contract Request', () => {
+describe('Create Contract Request', () => {
   useAppActors()
+
+  beforeEach(async ({ page }) => {
+    await page.addInitScript(() => {
+      window.localStorage.setItem('evmCollections', '')
+    })
+  })
 
   it(`with ${User} should able to create new request from chain explorer.`, async ({ actorCalled }) => {
     await actorCalled(User).attemptsTo(
       Navigate.to(laikaWebAppUrl),
-      ClickCreateNewRequestDialog(),
+      ClickCreateNewRequestDialog('Chain Explorer'),
       SelectChain(),
       FillContractAddrInput(),
+      SubmitCreateContractRequest(),
+      Ensure.that(
+        contractAddressInput.attribute('value'),
+        equals(notes<UserNote>().get('requestContract').contractAddress),
+      ),
+      Ensure.that(selectedChain.text(), equals(notes<UserNote>().get('requestContract').chain)),
+    )
+  })
+
+  it(`with ${User} should able to create new request by fill-in form.`, async ({ actorCalled }) => {
+    await actorCalled(User).attemptsTo(
+      Navigate.to(laikaWebAppUrl),
+      ClickCreateNewRequestDialog('New Request'),
+      SelectChain(),
+      FillContractAddrInput(),
+      FillAbi(),
       SubmitCreateContractRequest(),
       Ensure.that(
         contractAddressInput.attribute('value'),
