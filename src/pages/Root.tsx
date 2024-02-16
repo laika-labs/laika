9518464a91a085dcc34ffb0fa6810a017841c659
Nithi Nomprawat <0xnithi@gmail.com>
2024-02-16 21:25:30 +0700

    feat: add Announcement

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,7 @@
 import { Allotment } from 'allotment'
 import { Outlet } from 'react-router-dom'
 
+import Announcement from '@/components/Announcement'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Sidenav from '@/components/Sidenav'
@@ -8,6 +9,9 @@ import Sidenav from '@/components/Sidenav'
 export default function Root() {
   return (
     <Allotment className="w-screen h-screen" vertical>
+      <Allotment.Pane minSize={24} maxSize={24} className="flex">
+        <Announcement />
+      </Allotment.Pane>
       <Allotment.Pane minSize={48} maxSize={48} className="flex">
         <Header />
       </Allotment.Pane>
