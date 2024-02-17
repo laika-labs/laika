8b9b9ed7f986c702e655959cc586607c439eca02
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2024-02-17 21:15:57 +0700

    fix typo

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,7 +1,7 @@
 export default function Announcement() {
   return (
     <div className="w-full overflow-scroll text-sm text-center whitespace-nowrap">
-      Laika just got rewrote from ground up, But if you can access the legacy at{' '}
+      Laika just got rewrote from ground up, But you can access the legacy at{' '}
       <a href="https://legacy.getlaika.app" className="font-medium underline text-primary underline-offset-4">
         https://legacy.getlaika.app
       </a>
