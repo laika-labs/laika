0db6d029a7c983070e08882498489daf578d74e8
Nithi Nomprawat <0xnithi@gmail.com>
2024-02-18 21:48:29 +0700

    fix: Announcement scrollbar styling

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,6 +1,6 @@
 export default function Announcement() {
   return (
-    <div className="w-full overflow-scroll text-sm text-center whitespace-nowrap">
+    <div className="w-full overflow-x-auto text-sm text-center whitespace-nowrap no-scrollbar">
       Laika just got rewrote from ground up, But you can access the legacy at{' '}
       <a href="https://legacy.getlaika.app" className="font-medium underline text-primary underline-offset-4">
         https://legacy.getlaika.app
