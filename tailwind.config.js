69374d82dabdb8966e34c1ac611a686ba18e635f
tinguyen <ntrongtin11702@gmail.com>
2024-12-13 02:40:21 +0700

    fix: build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
@@ -67,5 +69,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
